73d0ed2b82f96e3409b056f0fb639f2fafffa34a
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-16 07:44:05 +0530

    feat: use effect to make a side effects

--- src/App.js
+++ src/App.js
@@ -1,52 +1,25 @@
 import './App.css';
-import { useState } from 'react';
-
-const counterInitialize = () => {
-  console.log('asdffffffffff'); // it called only one time
-  return 6
-}
+import { useState, useEffect } from 'react';
 
 function App() {
-   // if we put like this it will trigger every time when components re rendered
-  // const [counter, setCounter] = useState(counterInitialize());
-  // or suppose you want to run only the components initialize period we passed as arrow functions
-  const [counter, setCounter] = useState(() => {
-    console.log('asdffffffffff'); // it called only one time
-    return 6
-  });
+  const [resourceType, setResourceType] = useState(() => 'posts')
 
-  // for a complex data structure
-  const [data, setData] = useState(() => ({theme: 'dark', value: 1}));
-  // in order to avoid this
-  // we need to use seperate state like this
-  const [value, setvalue] = useState(() => 1);
-  const [theme, settheme] = useState(() => 'dark');
-
-
-  console.log('data', data)
-
-  function decrement() {
-    // setCounter(counter - 1); // 5
-    // // the current state will have to not updated here so we get 6
-    // setCounter(counter - 1);
-
-    // if you want to update the state based on the current state
-    setCounter((prevValue) => prevValue - 1)
-
-    // if i need to update particular property, we need to provided whole set of data
-    setData(prev => ({...prev, theme: 'blue'}))
-    // it will gives the current updated value
-    // setCounter((prevValue) => prevValue - 1)
-
-    
-  }
+  useEffect(() => {
+    console.log('this', resourceType)
+  }, [resourceType])
 
   return (
-    <div className="App">
-      <button onClick={decrement}>-</button>
-      <span>{counter}</span>
-      <span>{data.theme}</span>
-      <button>+</button>
+    <div>
+      <div>
+          <button onClick={() => setResourceType('posts')}>
+            Posts
+          </button>
+          <button  onClick={() => setResourceType('comments')}>Comments</button>
+          <button  onClick={() => setResourceType('users')}>Users</button>
+      </div>
+      <h3>
+        {resourceType}
+      </h3>
     </div>
   );
 }
