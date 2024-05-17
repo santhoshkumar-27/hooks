dd70a71f3bc0d71f0f1064960c90b47667dc27bd
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-17 07:05:28 +0530

    feat: use effect clean up previous subscription or event listener

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [resourceType, setResourceType] = useState(() => 'posts')
-  const [data, setData] = useState(() => '');
+  // const [resourceType, setResourceType] = useState(() => 'posts')
+  // const [data, setData] = useState(() => '');
+  const [windowWidth, setWindowWidth] = useState(() => window.innerWidth)
   // if we put as a dependices, once the changed the side effects will triggerd
-  useEffect(() => {
-    console.log('changed', resourceType)
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setData(JSON.stringify(json)))
-  }, [resourceType])
+  // useEffect(() => {
+  //   console.log('changed', resourceType)
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then(response => response.json())
+  //     .then(json => setData(JSON.stringify(json)))
+  // }, [resourceType])
   // But if you want do only mount time we need to use 
   // useEffect(() => {
   //   console.log('mount', resourceType)
   // }, [])
+
+  useEffect(() => {
+    const resized = () => setWindowWidth(window.innerWidth)
+    window.addEventListener('resize', resized);
+
+    return () => {
+      window.removeEventListener('resize', resized)
+    }
+  }, [])
   return (
     <div>
-      <div>
+      {/* <div>
         <button onClick={() => setResourceType('posts')}>
           Posts
         </button>
@@ -27,7 +37,9 @@ function App() {
       <h3>
         {resourceType}
       </h3>
-      <p>{data}</p>
+      <p>{data}</p> */}
+
+      {windowWidth}
     </div>
   );
 }
