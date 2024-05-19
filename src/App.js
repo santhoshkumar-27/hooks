a27ef379a69e57b4939fee8d0fa0709c6759d7d5
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-19 22:06:04 +0530

    feat: useRef started

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import React from 'react';
-import { FunctionContextComponent } from './FunctionContextComponent';
-import ThemeCustomProvider from './ThemeCustomContext';
+import React, { useState } from 'react';
 
 
 function App() {
-
+  const [state, setState] = useState(() => '');
   return (
-    <ThemeCustomProvider>
+    <>
+      <input value={state} onChange={e => setState(e.target.value)}/>
       <div>
-        <FunctionContextComponent />
+        My name is {state}
       </div>
-    </ThemeCustomProvider>
+    </>
   );
 }
 
