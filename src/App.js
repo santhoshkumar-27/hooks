ae532d40463716d0ffed0722510ec77b1e1344fb
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-19 22:08:19 +0530

    fix: set updated calls infinite

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
   const [state, setState] = useState(() => '');
+  const [rerenderCount, setReRenderCount] = useState(() => 0)
+
+  useEffect(() => {
+    setReRenderCount(prev => prev + 1);
+  });
+  // it throws the error of because it doing infinite set update calles 
   return (
     <>
       <input value={state} onChange={e => setState(e.target.value)}/>
