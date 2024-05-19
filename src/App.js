c051c061bf416cb159d5b31cd66a78c6b928374e
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-19 22:19:55 +0530

    useRef not managing state within render

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 
 function App() {
   const [state, setState] = useState(() => '');
-  const [rerenderCount, setReRenderCount] = useState(() => 0)
-
+  const rerenderCount = useRef(0)
+  console.log('rerenderCount', rerenderCount)
   useEffect(() => {
-    setReRenderCount(prev => prev + 1);
+    console.log('rerenderCount', rerenderCount)
+    rerenderCount.current += 1
   });
-  // it throws the error of because it doing infinite set update calles 
+  // useRef and useState
+  /*
+    UseState is takes a state and render a state in component, it chanages cause to render the component
+    but in the useRef it returns a object as current value, it doesn't trigger a rerender a compoent
+  */
   return (
     <>
       <input value={state} onChange={e => setState(e.target.value)}/>
       <div>
         My name is {state}
+      </div>
+      <div>
+        number of times renders {rerenderCount.current}
       </div>
     </>
   );
