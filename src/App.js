8f411878c503836c3ddcbec7f44c8e32b0d72776
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2024-05-20 16:36:06 +0530

    fix: changes of the use ref and to maintain local state and it didn't chance to trigger the rerender

--- src/App.js
+++ src/App.js
@@ -8,25 +8,37 @@ import React, { useEffect, useState, useRef } from 'react';
 function App() {
   const [state, setState] = useState(() => '');
   const rerenderCount = useRef(0)
-  console.log('rerenderCount', rerenderCount)
+  const inputRef = useRef();
+  const prevState = useRef('');
+  // console.log('rerenderCount', rerenderCount)
   useEffect(() => {
-    console.log('rerenderCount', rerenderCount)
+    // console.log('rerenderCount', rerenderCount)
     rerenderCount.current += 1
-  });
+    prevState.current = state;
+  }, [state]);
+
   // useRef and useState
   /*
     UseState is takes a state and render a state in component, it chanages cause to render the component
     but in the useRef it returns a object as current value, it doesn't trigger a rerender a compoent
   */
+  function focus() {
+    inputRef.current.focus();
+  }
+  // it only access the element
   return (
     <>
-      <input value={state} onChange={e => setState(e.target.value)}/>
+      <input ref={inputRef} value={state} onChange={e => setState(e.target.value)}/>
       <div>
         My name is {state}
       </div>
       <div>
         number of times renders {rerenderCount.current}
       </div>
+      <div>
+        previous renders {prevState.current}
+      </div>
+      <button onClick={focus}>Focus</button>
     </>
   );
 }
