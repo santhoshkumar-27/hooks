5baf2e472a0a502dcc652925c273d29b75eb7e00
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-21 12:05:12 +0530

    feat: main application done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+
+
+function reducer(state, action) {
+  return { count: state.count + 1 }
+}
 
 function App() {
-  const [number, setNumber] = useState(() => 0);
+  const [state, dispatch] = useReducer(reducer, { count: 0});
+  // since we object we use state, if use primitive value use related identifier name like count, know we are use
+  // state here
+  // dispatch funtion dispatch the action to the reducer, reducer function takes two arguments current state, and actions
+  // actions as two properties one is type and another one is value that needs to updated in the state and return new state
+  // const [number, setNumber] = useState(() => 0);
 
   function increment() {
-    setNumber(prev => prev + 1);
+    // setNumber(prev => prev + 1);
+    dispatch()
   }
 
   function decrement() {
-    setNumber(prev => prev - 1);
+    // setNumber(prev => prev - 1);
+    dispatch()
+    // dispatch function sended a action to the reducer take care part of applications
   }
   return (
     <div>
       <button onClick={increment}>+</button>
-      <span>{number}</span>
+      <span>{state.count}</span>
       <button onClick={decrement}>-</button>
     </div>
   );
