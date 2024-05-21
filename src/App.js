dfc258712a2f9631b6a8093e51d0e0a85c57f1ed
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-21 13:24:24 +0530

    feat: handle reducer functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 
 
 function reducer(state, action) {
-  return { count: state.count + 1 }
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return { ...state }
+  }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0});
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   // since we object we use state, if use primitive value use related identifier name like count, know we are use
   // state here
   // dispatch funtion dispatch the action to the reducer, reducer function takes two arguments current state, and actions
@@ -16,12 +23,12 @@ function App() {
 
   function increment() {
     // setNumber(prev => prev + 1);
-    dispatch()
+    dispatch({ type: 'increment' })
   }
 
   function decrement() {
     // setNumber(prev => prev - 1);
-    dispatch()
+    dispatch({ type: 'decrement' })
     // dispatch function sended a action to the reducer take care part of applications
   }
   return (
