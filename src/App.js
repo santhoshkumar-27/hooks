3805e9ff12c44a309fe24401f792920c8b1d2e26
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-21 14:10:49 +0530

    feat: initial of useTransistions

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
 import './App.css';
-import React, { useReducer } from 'react';
+import React, { useState, useTransition } from 'react';
 
-const ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement'
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1 };
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      return { ...state }
-  }
-}
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  // since we object we use state, if use primitive value use related identifier name like count, know we are use
-  // state here
-  // dispatch funtion dispatch the action to the reducer, reducer function takes two arguments current state, and actions
-  // actions as two properties one is type and another one is value that needs to updated in the state and return new state
-  // const [number, setNumber] = useState(() => 0);
+  const [isPending, startTansistions] = useTransition();
+  const [state, setState] = useState(() => '');
+  const [list, setList] = useState(() => []);
+  const list_size = 2000;
 
-  function increment() {
-    // setNumber(prev => prev + 1);
-    dispatch({ type: ACTIONS.INCREMENT })
-  }
+  function onChange(value) {
+    setState(value)
 
-  function decrement() {
-    // setNumber(prev => prev - 1);
-    dispatch({ type: ACTIONS.DECREMENT })
-    // dispatch function sended a action to the reducer take care part of applications
+    let l = [];
+    for (let i = 1; i < list_size; i++) {
+      l.push(value);
+    }
+
+    setList(l)
   }
   return (
     <div>
-      <button onClick={increment}>+</button>
-      <span>{state.count}</span>
-      <button onClick={decrement}>-</button>
+      <input type="text" name="sdfasf" id="asdf" value={state} onChange={(e) => onChange(e.target.value)} />
     </div>
   );
 }
