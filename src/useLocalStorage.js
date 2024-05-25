3647219816448c24ec922fda2dd3d8a6d4e6cc42
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-25 11:12:08 +0530

    this will be different format

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -26,6 +26,13 @@ export default function useLocalStorage(key, initialValue) {
     // useDebbugValue works only inside the custom hooks
 
     useDebugValue(value) // show the value in the label next to the custom hook name
+    useDebugValue(key); // if we put n of this we can see this in the array format
+    // e.g LocalStorage ["sdfdsf", "firstName"]
+
+    // or we can put llike this
+    useDebugValue([key, {key: value}])
+
+    useDebugValue( {key: value})
     // it available for development and production
 
     // to show the state of this hook
