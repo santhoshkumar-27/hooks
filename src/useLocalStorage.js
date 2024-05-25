a81f08d32b89a46211cff3df10506d76a0c7e0d4
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-25 11:28:06 +0530

    feat: know of use debug value

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -25,17 +25,30 @@ export default function useLocalStorage(key, initialValue) {
 
     // useDebbugValue works only inside the custom hooks
 
-    useDebugValue(value) // show the value in the label next to the custom hook name
-    useDebugValue(key); // if we put n of this we can see this in the array format
+    // useDebugValue(value) // show the value in the label next to the custom hook name
+    // useDebugValue(key); // if we put n of this we can see this in the array format
     // e.g LocalStorage ["sdfdsf", "firstName"]
 
     // or we can put llike this
-    useDebugValue([key, {key: value}])
+    // useDebugValue([key, {key: value}])
 
-    useDebugValue( {key: value})
+    // useDebugValue( {key: value})
     // it available for development and production
 
     // to show the state of this hook
 
+
+    // useDebugValue(getSlowFunction(value)) // it runs every time in productions alsow
+
+    useDebugValue(value, v => getSlowFunction(v)) // but if we want to run only in the develope tool open and react developer tool
+    // during this time it will not run this functios
+    // it will act us slow function and not responding to user action
     return [value, setValue]
+}
+
+
+function getSlowFunction(value) {
+    for (let i = 0; i < 3000000000; i++) {};
+
+    return value;
 }
