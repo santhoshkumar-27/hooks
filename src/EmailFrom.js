ec26a5e399acc80b33c5914241750c27c0121801
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-25 17:32:08 +0530

    feat: message

--- src/EmailFrom.js
+++ src/EmailFrom.js
@@ -2,13 +2,13 @@ import { useId } from "react"
 
 export function EmailFrom() {
     const id = useId();
-    // it will give same id when its render but in the same order before
+    // it will give same id when its re-render after the initial render of ui but in the same order before
     // if we use dom api to mainipulate these it will not work
     // we need to use of useRef
     return (
         <>
-            <label htmlFor={id}>Email</label>
-            <input id={id} name="email" />
+            <label htmlFor={`${id}-email`}>Email</label>
+            <input id={`${id}-email`} name="email" />
         </>
     )
 }
